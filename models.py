aeaad231c159d7593701ee59ef194a4f30a708e8
Pranjali <105532022+Pranjali31@users.noreply.github.com>
2023-08-10 20:26:29 -0400

    Update models.py

--- models.py
+++ models.py
@@ -28,10 +28,9 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
     
-    # Add related_name arguments to avoid conflicts
     groups = models.ManyToManyField(
      'auth.Group',
-     related_name='custom_user_set',  # Add a unique related_name for groups
+     related_name='custom_user_set',  
      blank=True,
      help_text='The groups this user belongs to. A user will get all permissions '
                'granted to each of their groups.',
@@ -39,7 +38,7 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
     )
     user_permissions = models.ManyToManyField(
      'auth.Permission',
-     related_name='custom_user_set',  # Add a unique related_name for user_permissions
+     related_name='custom_user_set',  
      blank=True,
      help_text='Specific permissions for this user.',
      verbose_name='user permissions',
